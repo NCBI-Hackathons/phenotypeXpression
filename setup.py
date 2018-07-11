e25f4f76700f8d66e9289def39e4373a2b6ce87d
Lucy Lu Wang <lucee25@gmail.com>
2018-07-11 11:41:42 -0500

    setup files

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import setuptools
+
+setuptools.setup(
+    name='phenox',
+    version='0.1',
+    url='https://github.com/NCBI-Hackathons/phenotypeXpression',
+    packages=setuptools.find_packages(),
+    install_requires=[
+    ],
+    tests_require=[
+    ],
+    zip_safe=False,
+    test_suite='py.test',
+    entry_points='',
+)
---
+++ setup.sh
@@ -0,0 +1,32 @@
+CONDAENV=phenoX
+
+if ! (which wget); then
+    echo "No `wget` installation found. Please install wget first."
+    exit 1
+fi
+
+if ! (which conda); then
+	echo "No `conda` installation found.  Installing..."
+	if [[ $(uname) == "Darwin" ]]; then
+	  wget --continue http://repo.continuum.io/archive/Anaconda3-4.3.1-MacOSX-x86_64.sh
+	  bash Anaconda3-4.3.1-MacOSX-x86_64.sh -b
+	else
+	  wget --continue http://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+	  bash Anaconda3-4.3.1-Linux-x86_64.sh -b
+	fi
+fi
+
+export PATH=$HOME/anaconda3/bin:$PATH
+
+source ~/anaconda3/bin/deactivate ${CONDAENV}
+
+conda remove -y --name ${CONDAENV} --all
+
+conda create -n ${CONDAENV} -y python==3.6 pip pytest || true
+
+echo "Activating Conda Environment ----->"
+source ~/anaconda3/bin/activate ${CONDAENV}
+
+pip install -r requirements.txt
+
+python setup.py develop
