0998361b598979118ccb024ee69b1600fea2704c
baoxiaojun43@gmail.com <baoxiaojun43@gmail.com>
2018-07-13 11:39:13 -0500

    add title and keywords

--- phenox/pubmed.py
+++ phenox/pubmed.py
@@ -42,11 +42,28 @@ class Pubmed:
     def fetch_abstract(self, pmid):
         handle = Entrez.efetch(db='pubmed', id=pmid, retmode='xml')
         record = Entrez.read(handle)
+        abs_str = []
+        # abstract text
         try:
-            abstract = record['PubmedArticle'][0]['MedlineCitation']['Article']['Abstract']['AbstractText'][0]
+            abstract = record['PubmedArticle'][0]['MedlineCitation']['Article']['Abstract']['AbstractText']
+            for a in abstract:
+                abs_str.append(a)
         except:
-            abstract = ''
-        self.pmid_abstracts[pmid] = str(abstract)
+            abs_str.append('')
+        # title
+        try:
+            title = record['PubmedArticle'][0]['MedlineCitation']['Article']['ArticleTitle']
+            abs_str.append(title)
+        except:
+            abs_str.append('')
+        # keyword list
+        try:
+            kw = record['PubmedArticle'][0]['MedlineCitation']['KeywordList'][0]
+            for w in kw:
+                abs_str.append(w)
+        except:
+            abs_str.append('')
+        self.pmid_abstracts[pmid] = ' '.join(abs_str)
         
     def extract_DNER(self, pmid):
         self.pmid_ent_text[pmid], self.pmid_dner[pmid] = self.find_DNER(self.pmid_abstracts[pmid])
