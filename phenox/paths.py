8b7ee347066bae62caa0b5b4ea87776d3d172433
Lucy Lu Wang <lucee25@gmail.com>
2018-07-26 16:23:15 -0700

    fixed path problem

--- phenox/paths.py
+++ phenox/paths.py
@@ -3,7 +3,8 @@ import os
 
 # class with phenox directories
 class PhenoXPaths:
-    def __init__(self, base_dir="~/git/phenotypeXpression/"):
+    def __init__(self, base_dir=os.getcwd()):
+        assert os.path.exists(os.path.join(base_dir, 'PhenoX.png'))
         self.base_dir = os.path.expanduser(base_dir)
         self.data_dir = os.path.join(self.base_dir, 'data')
         self.src_dir = os.path.join(self.base_dir, 'phenox')
