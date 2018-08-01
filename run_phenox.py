87da28fb3da46b586d1287518bbc46ecf88dd5cb
Lucy Lu Wang <lucee25@gmail.com>
2018-07-31 22:42:04 -0700

    adding rob's code for argparse

--- run_phenox.py
+++ run_phenox.py
@@ -2,6 +2,8 @@
 
 import argparse
 from phenox.phenox import PhenoX
+
+
 __version__ = '0.1.0'
 
 
@@ -10,33 +12,41 @@ def run(args):
     phenox = PhenoX(args.email, args.query_str)
     phenox.subtype()
 
-# argparse function for options can include or delete
-# def getargs():
-#     parser = argparse \
-#             .ArgumentParser(prog='run_phenox.py',
-#                             formatter_class=argparse.RawTextHelpFormatter,
-#                             description='Subclassification of disease states '
-#                                         'based on the intersection of literat'
-#                                         'ure and expression')
-#     parser.add_argument('-o', metavar='prefix', dest='outprefix',
-#                         default='phenox',
-#                         help='choose an alternate prefix for outfiles')
-#     parser.add_argument("--version", action='version',
-#                         version='\n'.join(['PhenoX v' + __version__]))
-#     parser.add_argument("query", help="disease query term")
-#     requiredNamed = parser.add_argument_group('required arguments')
-#     requiredNamed \
-#             .add_argument('-e', dest='email', required=True,
-#                           help='NCBI requires an email for database queries')
-#     return parser.parse_args()
 
-parser = argparse.ArgumentParser()
+# argparse function for options
+def getargs():
+    parser = argparse.ArgumentParser(
+        prog='run_phenox.py',
+        formatter_class=argparse.RawTextHelpFormatter,
+        description='Subclassification of disease states based on the '
+                    'intersection of literature and expression'
+    )
 
-parser.add_argument('email')
-parser.add_argument('query_str')
-parser.set_defaults(func=run)
+    parser.add_argument(
+        '-o', metavar='prefix', dest='outprefix',
+        default='phenox',
+        help='choose an alternate prefix for outfiles'
+    )
+
+    parser.add_argument(
+        "--version", action='version',
+        version='\n'.join(['PhenoX v' + __version__])
+    )
+
+    parser.add_argument(
+        "query_str", help="disease query term"
+    )
+
+    required_named = parser.add_argument_group('required arguments')
+
+    required_named.add_argument(
+        '-e', dest='email', required=True,
+        help='NCBI requires an email for database queries'
+    )
+
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
-    # args = getargs() # if using getargs function
-    args = parser.parse_args()
-    args.func(args)
+    args = getargs()
+    run(args)
