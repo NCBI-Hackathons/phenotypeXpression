95f15d1e90d82d18e05c14b108eb531cce8a8da9
Lucy Lu Wang <lucee25@gmail.com>
2018-10-04 14:19:54 -0700

    moved tqdm to show PubMed batch queries

--- phenox/phenox.py
+++ phenox/phenox.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import math
+import tqdm
 from typing import List, Dict, Tuple
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
@@ -88,7 +89,8 @@ class PhenoX:
         for clust, gene_names in cluster_to_gene_name.items():
             query_terms = pubmed.construct_query_terms(mesh_term['name'], gene_names)
             pubmed_clust = []
-            for query_term in query_terms:
+            sys.stdout.write('Querying PubMed for genes from {}\n'.format(clust))
+            for query_term in tqdm.tqdm(query_terms, desc='PubMed batches'):
                 pubmed_clust += geo.get_ncbi_docsum(mesh_term['name'], "pubmed", query_term)
             pubmed_ids[clust] += base_utils.flatten(pubmed_clust)
 
