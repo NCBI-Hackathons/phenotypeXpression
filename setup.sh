1582f3f9f5994472559f71293a13ff2e7a5c4480
rbutleriii <rbutler@northshore.org>
2018-11-03 17:07:37 -0500

    setup script fixes

--- setup.sh
+++ setup.sh
@@ -10,11 +10,11 @@ fi
 if ! (which conda); then
 	echo "No `conda` installation found.  Installing..."
 	if [[ $(uname) == "Darwin" ]]; then
-	  wget --continue http://repo.continuum.io/archive/Anaconda3-4.4.0-MacOSX-x86_64.sh
-	  bash Anaconda3-4.4.0-MacOSX-x86_64.sh -b
+	  wget --continue http://repo.continuum.io/archive/Anaconda3-4.3.1-MacOSX-x86_64.sh
+	  bash Anaconda3-4.3.1-MacOSX-x86_64.sh -b
 	else
-	  wget --continue http://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh
-	  bash Anaconda3-4.4.0-Linux-x86_64.sh -b
+	  wget --continue http://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+	  bash Anaconda3-4.3.1-Linux-x86_64.sh -b
 	fi
     printf '\n# Anaconda path addition\n' >> $HOME/.bashrc
     echo 'export PATH="$HOME/anaconda3/bin:$PATH"' >> $HOME/.bashrc
@@ -22,7 +22,6 @@ fi
 
 CONDAPATH="$(which conda)" || CONDAPATH=$HOME/anaconda3/bin
 echo "CONDAPATH => $CONDAPATH"
-
 export PATH=$CONDAPATH:$PATH
 
 conda create -n ${CONDAENV} -y python==3.6 pip pytest || true
